e4e8747c93f8087e728e37748fdde31719752909
Zavents <pku.mca16@gmail.com>
2025-10-17 16:06:07 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
 export default defineConfig({
+  // ⬅️ ADD THIS LINE
+  base: "/react-bendahara/",
+  // ⬆️ ADD THIS LINE
   plugins: [react()],
-})
+});
