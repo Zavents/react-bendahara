3f789521644f890f6b1b551b5da97a0958e7a912
Zavents <pku.mca16@gmail.com>
2025-10-17 16:10:17 +0700

    fix: updated vite base path and rebuilt for gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-// vite.config.js
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// [https://vitejs.dev/config/](https://vitejs.dev/config/)
 export default defineConfig({
-  // ⬅️ ADD THIS LINE
-  base: "/react-bendahara/",
-  // ⬆️ ADD THIS LINE
+  // ⬅️ CRITICAL FIX: The base path must be your repo name
+  base: "/react-bendahara/", 
   plugins: [react()],
 });
